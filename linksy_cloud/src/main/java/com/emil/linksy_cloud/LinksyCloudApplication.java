845659eada6e0662174f3167816f0d40f4f09b8c
emil <milbgr@gmail.com>
2024-11-26 16:38:48 +0300

    added a microservice сloud

---
+++ linksy_cloud/src/main/java/com/emil/linksy_cloud/LinksyCloudApplication.java
@@ -0,0 +1,13 @@
+package com.emil.linksy_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksyCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksyCloudApplication.class, args);
+	}
+
+}
---
+++ linksy_cloud/src/test/java/com/emil/linksy_cloud/LinksyCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emil.linksy_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksyCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
