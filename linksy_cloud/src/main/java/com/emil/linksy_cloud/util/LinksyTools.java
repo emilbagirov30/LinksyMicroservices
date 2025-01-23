55fd0b8a83d4ca0bd820b9ea002b8e23582e9596
emil <milbgr@gmail.com>
2025-01-23 15:45:35 +0300

    correct error: The multipart replaced the line break with "\n"

--- linksy_cloud/src/main/java/com/emil/linksy_cloud/util/LinksyTools.java
+++ linksy_cloud/src/main/java/com/emil/linksy_cloud/util/LinksyTools.java
@@ -3,6 +3,6 @@ package com.emil.linksy_cloud.util;
 public class LinksyTools {
 
     public static String clearQuotes (String source){
-        return source.substring(1,source.length()-1);
+        return source.substring(1,source.length()-1).replace("\\n", "\n");
     }
 }
